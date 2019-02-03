59ecfdb67f793e4a390bc343c7f8d94cd39e4c87
John Naylor <jonaylor89@gmail.com>
2019-02-03 02:45:09 -0500

    it searches and downloads.

--- controllers/YoutubeController.js
+++ controllers/YoutubeController.js
@@ -7,23 +7,10 @@ module.exports.grabInstrumental = function(title) {
   // lol
   title += " instrumental"
 
-  let opts = {
-    maxResults: 1,
-    key: 'AIzaSyArKNxoDuAkrTGXKrk9nFnAPseRX1xDStM'
-  };
- 
-  return search(title, opts, function(err, results) {
-    if(err) return console.log(err);
- 
-      console.log(results);
+  ydl.exec("ytsearch1:" + title, ['-o', title + '.mp3', '-x', '--audio-format', 'mp3'], {}, function(err, output) {
+    if (err) throw err;
 
-      let url = results[0];
-
-      ytdl.exec(url, ['-o', title + '.mp3', '-x', '--audio-format', 'mp3'], {}, function(err, output) {
-        if (err) throw err;
-
-        console.log(output.join('\n'));
-      });   
-  });
+      console.log("OUTPUT " + output.join('\n'));
+    });   
   
 };
