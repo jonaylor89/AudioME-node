69374257f46d785080030cf2c3ecf10e338b2e12
John Naylor <jonaylor89@gmail.com>
2019-02-03 08:26:18 -0500

    Things

--- controllers/YoutubeController.js
+++ controllers/YoutubeController.js
@@ -7,7 +7,7 @@ module.exports.grabInstrumental = function(title) {
   // lol
   title += " instrumental"
 
-  ydl.exec("ytsearch1:" + title, ['-o', title + '.mp3', '-x', '--audio-format', 'mp3'], {}, function(err, output) {
+  ydl.exec("ytsearch1:" + title, ['-o', 'music.mp3', '-x', '--audio-format', 'mp3'], {}, function(err, output) {
     if (err) throw err;
 
       console.log("OUTPUT " + output.join('\n'));
